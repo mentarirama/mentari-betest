9af938e4ea62754b4716c860c143cd4ed34bbcba
mentarirama <mentarirama@gmail.com>
2022-06-26 23:17:50 +0700

    add read, update, delete func

--- routes/test.js
+++ routes/test.js
@@ -17,10 +17,48 @@ router.post('/', async (req, res) => {
     })
 
     try {
-        const test = await testPost.save()
-        res.json(test)
+        const testCreate = await testPost.save() //method mongodb
+        res.json(testCreate)
     } catch (err) {
         res.json({message: err}) 
+    }
+})
+
+// READ 
+router.get('/', async (req, res) => {
+    try {
+        const testRead = await user.find() //method mongodb
+        res.json(testRead) 
+    } catch (err) {
+        res.json({message: err})
+        
+    }
+})
+
+// UPDATE
+router.put('/:userId', async (req, res) => {
+    try {
+        const testUpdate = await user.updateOne({_id: req.params.userId}, {
+            id: req.body.id,
+            userName: req.body.userName,
+            accountNumber: req.body.accountNumber,
+            emailAddress: req.body.emailAddress,
+            identityNumber: req.body.identityNumber
+        })
+        res.json(testUpdate)
+    } catch (err) {
+        res.json({message: err})
+        
+    }
+})
+
+// DELETE
+router.delete('/:userId', async (req, res) => {
+    try {
+        const testUpdate = await user.deleteOne({_id: req.params.userId})
+        res.json(testUpdate)
+    } catch (err) {
+        res.json({message: err})
     }
 })
 
